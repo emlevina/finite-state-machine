b28d94b01a0f457cf2a5fab8918cad1ddfcaffc8
Ekaterina Orlova <katya.leemi@gmail.com>
2024-04-11 14:44:13 +0300

    Updated file strucure, added favicon

--- sample-app/webpack.config.js
+++ sample-app/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: process.env.NODE_ENV || "development",
@@ -22,7 +23,10 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "public", to: "static" }],
     }),
   ],
   devServer: {
