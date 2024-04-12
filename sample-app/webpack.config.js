4107b056dcc9b242d35ca0b9dde60dc031d37862
Ekaterina Orlova <katya.leemi@gmail.com>
2024-04-12 17:44:02 +0300

    Fix

--- sample-app/webpack.config.js
+++ sample-app/webpack.config.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 const path = require("path");
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
