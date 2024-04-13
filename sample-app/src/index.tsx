08999451cd0c8349d38c1c9f3be55a503b96de3b
Ekaterina Orlova <katya.leemi@gmail.com>
2024-04-13 09:30:33 +0300

    Deleted normalize

--- sample-app/src/index.tsx
+++ sample-app/src/index.tsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./store";
-import "normalize.css";
 import "./themes/reset.css";
 import "./themes/themes.scss";
 
