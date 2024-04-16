2d5db9e8016466bf3b7784d4ccccbcfe03f1ebf8
Ekaterina Orlova <katya.leemi@gmail.com>
2024-04-16 07:40:58 +0300

    Refactored handlers.ts to remove unused code

--- sample-app/src/mocks/handlers.ts
+++ sample-app/src/mocks/handlers.ts
@@ -1,22 +1,12 @@
 import { http, HttpResponse, passthrough } from "msw";
 
 export const handlers = [
-  http.get("https://numbersapi.p.rapidapi.com/:number/trivia", ({ params }) => {
-    const { number } = params;
-    if (number === "42") {
-      return HttpResponse.error();
-    }
-    if (number === "12") {
-      return passthrough();
-    }
-    return HttpResponse.text("This number is very very special");
-  }),
   http.get("http://localhost:3000/api/facts/:number", ({ params }) => {
     const { number } = params;
     if (number === "42") {
       return HttpResponse.error();
     }
-    if (number === "12") {
+    if (number === "12" || number === "188") {
       return passthrough();
     }
     return HttpResponse.json({ fact: "This number is very very special" });
