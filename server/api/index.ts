54afb9fdd71b941c757522aec86466be2c9f75e6
Ekaterina Orlova <katya.leemi@gmail.com>
2024-04-16 09:05:04 +0300

    Added Swagger documentation for Facts API

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,9 +1,15 @@
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import swaggerUi from "swagger-ui-express";
 import { factsRouter } from "./routers/facts.js";
 import { connectToDB } from "./middleware/connectToDB.js";
+import { swaggerDocs } from "./swagger/config.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 dotenv.config();
 
@@ -17,7 +23,9 @@ app.use(
     credentials: true,
   })
 );
-app.use(connectToDB);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api", connectToDB);
 app.use("/api/facts", factsRouter);
 
 export default app;
